2b117784540506acb513ce3c807fbdfc19d4034b
Alfredo Faria <alfredo.faria88@gmail.com>
2018-04-14 00:47:38 +0000

    UI Done

--- script/main.js
+++ script/main.js
@@ -1,8 +1,10 @@
-const buttonsContainer = document.getElementById('buttonsContainer');
+/*const buttonsContainer = document.getElementById('buttonsContainer');
 
 for (var i = 0; i<16; i++) {
   const button = document.createElement('div');
+  const char = document.createElement('p');
   button.classList = 'calcButton';
   button.classList.add('button' + i);
   buttonsContainer.appendChild(button);
 }
+*/
