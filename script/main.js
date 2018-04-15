f2dd628f2e68f82b8a5294fd25b5bb16437cf031
Alfredo Faria <alfredo.faria88@gmail.com>
2018-04-15 15:49:44 +0000

    display numbers + clean

--- script/main.js
+++ script/main.js
@@ -1,10 +1,50 @@
-/*const buttonsContainer = document.getElementById('buttonsContainer');
+var displayValue = '0';
+var firstNum = "";
+var secondNum = "";
+var operator = undefined;
 
-for (var i = 0; i<16; i++) {
-  const button = document.createElement('div');
-  const char = document.createElement('p');
-  button.classList = 'calcButton';
-  button.classList.add('button' + i);
-  buttonsContainer.appendChild(button);
+const screen = document.querySelector('.screen');
+screen.textContent = displayValue;
+
+const cleaner = document.getElementById('clear');
+cleaner.addEventListener('click', clean);
+
+function clean () {
+  displayValue = 0;
+  firstNum = 0;
+  secondNum = 0;
+  operator = undefined;
+  updateDisplay();
 }
-*/
+
+
+ for (var i = 0; i < 10; i++){
+ const num = document.querySelectorAll('.num');
+ num[i].addEventListener("click", operate);
+ }
+
+function operate(e) {
+  var num = e.target.textContent;
+  displayValue += num;
+  updateDisplay();
+}
+
+function updateDisplay () {
+  screen.textContent = displayValue;
+}
+
+function add(a, b) {
+	return a + b;
+}
+
+function subtract(a, b) {
+	return a - b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function divide(a, b) {
+	return a / b;
+}
